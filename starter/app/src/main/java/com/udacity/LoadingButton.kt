88fcbb84ee0b424fa66752015a406298172e9afb
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-06-25 15:10:06 +0200

    Add rectangle animation

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,11 +1,14 @@
 package com.udacity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Typeface
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
@@ -13,13 +16,34 @@ import kotlin.properties.Delegates
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    init {
+        isClickable = true
+    }
+
     private var widthSize = 0
     private var heightSize = 0
 
-    private val valueAnimator = ValueAnimator()
+    private var currentLoadingPercentage = 0.0f
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var valueAnimator: ValueAnimator? = null
 
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { _, _, new ->
+        when (new) {
+            ButtonState.Clicked -> {
+                buttonState = ButtonState.Loading
+            }
+
+            ButtonState.Loading -> {
+                isClickable = false
+                buttonText = context.resources.getString(R.string.button_loading)
+                startLoadingAnimation()
+            }
+
+            ButtonState.Completed -> {
+                isClickable = true
+                buttonText = context.resources.getString(R.string.button_name)
+            }
+        }
     }
 
     private var buttonText = resources.getString(R.string.button_name)
@@ -31,17 +55,43 @@ class LoadingButton @JvmOverloads constructor(
     }
 
     //
+    // Animation
+    //
+    private fun startLoadingAnimation() {
+        valueAnimator = ValueAnimator.ofFloat(0.0f, 1.0f)
+        valueAnimator?.apply {
+            duration = 5000
+
+            addUpdateListener { animation ->
+                currentLoadingPercentage = animation.animatedValue as Float
+                invalidate()
+            }
+
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    currentLoadingPercentage = 0.0f
+                    buttonState = ButtonState.Completed
+                }
+            })
+
+            start()
+        }
+    }
+
+    //
     // onDraw
     //
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.let {
-            drawButton(it)
+            drawRectangle(it)
             drawText(it)
+
+            drawLoadingRectangle(it)
         }
     }
 
-    private fun drawButton(canvas: Canvas) {
+    private fun drawRectangle(canvas: Canvas) {
         paint.color = ContextCompat.getColor(context, R.color.colorPrimary)
         canvas.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
     }
@@ -49,6 +99,11 @@ class LoadingButton @JvmOverloads constructor(
     private fun drawText(canvas: Canvas) {
         paint.color = ContextCompat.getColor(context, R.color.white)
         canvas.drawText(buttonText, widthSize / 2.0f, heightSize / 2.0f + 20.0f, paint)
+    }
+
+    private fun drawLoadingRectangle(canvas: Canvas) {
+        paint.color = ContextCompat.getColor(context, R.color.colorPrimaryDark)
+        canvas.drawRect(0f, 0f, widthSize.toFloat() * currentLoadingPercentage, heightSize.toFloat(), paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -62,6 +117,16 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
+    }
+
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+
+        buttonState = ButtonState.Clicked
+
+
+        invalidate()
+        return true
     }
 
 }
